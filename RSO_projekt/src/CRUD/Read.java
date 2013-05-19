2cad6a4c7ed9d3f6c795a24018293f1a2b05176d
mrosiew <m.rosiewicz@gmail.com>
2013-05-19 16:51:09 +0200

    Przymierzenie do sprawdzania warunkow w zapytaniu
    TODO: trzeba zrobić przechowywanie informacji o typie danej w środku
    BSONElement, to będzie wygodniejsze niż użycie refleksji

--- RSO_projekt/src/CRUD/Read.java
+++ RSO_projekt/src/CRUD/Read.java
@@ -1,52 +1,52 @@
 package CRUD;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import com.google.gson.Gson;
-import com.mongodb.DBObject;
-import java.util.List;
-import bson.BSONDocument;
-import bson.BSON;
-import messages.QueryMessage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.CharBuffer;
 import java.util.LinkedList;
+import java.util.List;
+
+import messages.QueryMessage;
+import bson.BSON;
+import bson.BSONDocument;
 
 public class Read {
 	public static List<BSONDocument> ReadFromQuery(QueryMessage queryMessage)
 	{
 		File[] fileArray = FileOperations.openCollection(queryMessage.fullCollectionName);
-		
 		List<BSONDocument> documentList = new LinkedList<BSONDocument>();
 		
 		for(int i = 0; i<fileArray.length; i++)
 		{
-			documentList.add(ReadFromFile(queryMessage, fileArray[i]));
+			BSONDocument bsonDocument = ReadFromFile(queryMessage, fileArray[i]);
+			if(bsonDocument.checkQuery(queryMessage.query))
+			{
+				documentList.add(bsonDocument);
+			}
 		}
 		
 		return documentList;
 	}
-	
+
 	public static BSONDocument ReadFromFile(QueryMessage message, File file)
 	{
 		long longNumber = file.length();
 		char[] array = new char[(int) longNumber];
 		
-		try {
+		try 
+		{
 			FileReader fileReader = new FileReader(file);
 			fileReader.read(array);
 			fileReader.close();
-		} catch (IOException e) {
+		} 
+		catch (IOException e) 
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		BSONDocument bsonDocument = new BSONDocument();
 		BSON.parseBSON(array, bsonDocument);
-		
 		return bsonDocument;
 	}
 
--- RSO_projekt/src/bson/BSONDocument.java
+++ RSO_projekt/src/bson/BSONDocument.java
@@ -1,6 +1,7 @@
 package bson;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class BSONDocument {
@@ -10,4 +11,27 @@ public class BSONDocument {
 	{
 		elems = new ArrayList<BSONElement<?>>();
 	}
+	
+	public Boolean checkQuery(BSONDocument query)
+	{
+		Iterator<BSONElement<?>> iterator = query.elems.iterator();
+		
+		while(iterator.hasNext())
+		{
+			BSONElement<?> queryElement = iterator.next();
+			if(!checkCondition(queryElement))
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private Boolean checkCondition(BSONElement<?> condition)
+	{
+		//TODO napisaæ
+		return true;
+	}
+	
 }
