cd239912bedc8c1a90cfd2603c433382c61e42ae
Tomasz Adamiec <adamiec.tomas@gmail.com>
2013-05-28 10:29:55 +0200

    1. poprawiony Delete 2. dodany folder z testowymi Bsonami 3. pozostalo do dodania usuwanie po innych parametrach niz name

--- RSO_projekt/src/CRUD/Delete.java
+++ RSO_projekt/src/CRUD/Delete.java
@@ -4,82 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 import messages.DeleteMessage;
-import messages.QueryMessage;
-
 import bson.BSON;
 import bson.BSONDocument;
 import bson.BSONElement;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import com.google.gson.Gson;
-import com.mongodb.DBObject;
-
 public class Delete {
-	public void deleteObject(final Collection<DBObject> collection, final int id) {
-
-		final Gson gson = new Gson();
-		DBObject dbo = Iterables.find(collection, new Predicate<DBObject>() {
-			public boolean apply(DBObject dbo) {
-				// System.out.println(dbo);
-				DataObject dataObject = gson.fromJson(dbo.toString(),
-						DataObject.class);
-				if (dataObject.getId() == id) {
-					// System.out.println(id);
-					// usun element
-					collection.remove(dbo);
-				}
-				return dataObject.getId() == id;
-			}
-		});
-	}
-
-	public void deleteObject(final Collection<DBObject> collection,
-			final String name) {
-
-		final Gson gson = new Gson();
-		DBObject dbo = Iterables.find(collection, new Predicate<DBObject>() {
-			public boolean apply(DBObject dbo) {
-				// System.out.println(dbo);
-				DataObject dataObject = gson.fromJson(dbo.toString(),
-						DataObject.class);
-				if (dataObject.getName() == name) {
-					// System.out.println(id);
-					// usun element
-					collection.remove(dbo);
-				}
-				return dataObject.getName() == name;
-			}
-		});
-	}
-
-	public void deleteObject(final Collection<DBObject> collection,
-			final double value) {
-
-		final Gson gson = new Gson();
-		DBObject dbo = Iterables.find(collection, new Predicate<DBObject>() {
-			public boolean apply(DBObject dbo) {
-				// System.out.println(dbo);
-				DataObject dataObject = gson.fromJson(dbo.toString(),
-						DataObject.class);
-				if (dataObject.getValue() == value) {
-					// System.out.println(id);
-					// usun element
-					collection.remove(dbo);
-				}
-				return dataObject.getValue() == value;
-			}
-		});
-	}
 
 	public void deteleEntry(DeleteMessage deleteMessage) {
+		// pobieranie danych z deleteMessage
 		BSONDocument deleteBsonDocument = deleteMessage.selector;
 		Iterator<BSONElement<?>> iterator = deleteBsonDocument.getElems()
 				.iterator();
@@ -91,22 +26,32 @@ public class Delete {
 				File[] fileArray = FileOperations
 						.openCollection(deleteMessage.fullCollectionName);
 				for (int i = 0; i < fileArray.length; i++) {
-					BSONDocument fileBsonDocument = readFromFile(fileArray[i]);
-					Iterator<BSONElement<?>> iterator2 = fileBsonDocument
-							.getElems().iterator();
-					// przeszukiwanie BSON-ow w plikach
-					while (iterator.hasNext()) {
-						BSONElement<?> searchElement = iterator2.next();
-						if (deleteElement.getName() == searchElement.getName()) {
-							System.out
-									.println("znaleziono dokument z t¹ sam¹ nazw¹ w pliku"
-											+ deleteMessage.fullCollectionName);
-							// sprwadzenie inncyh wariantow
-						}
-					}
+					// przeszukiwanie wszystkich plikow
+					compareWithFile( fileArray[i],  deleteElement,  deleteMessage);
 				}
 			}
+		}
+	}
 
+	public void compareWithFile(File file, BSONElement<?> deleteElement,
+			DeleteMessage deleteMessage) {
+		// pobieranie danych z pliku
+		BSONDocument fileBsonDocument = readFromFile(file);
+		Iterator<BSONElement<?>> iterator2 = fileBsonDocument.getElems()
+				.iterator();
+		// przeszukiwanie BSON-ow w pliku
+		while (iterator2.hasNext()) {
+			BSONElement<?> searchElement = iterator2.next();
+			// porownanie nazwy z deleteMessage i z pliku
+			if (deleteElement.getName().equals(searchElement.getName())) {
+				System.out.println("znaleziono dokument z t¹ sam¹ nazw¹ "
+						+ searchElement.getName() + " w pliku "
+						+ deleteMessage.fullCollectionName);
+				System.out.println("zawartoœæ: " + searchElement.getData());
+			}
+			
+			// TODO:
+			// dodac inne opcje (usuwanie nie tylko po nazwie)
 		}
 	}
 
@@ -115,15 +60,6 @@ public class Delete {
 		char[] array = new char[(int) longNumber];
 		String[] tablica = new String[(int) longNumber];
 		StringBuilder fileContent = new StringBuilder();
-
-		// try {
-		// FileReader fileReader = new FileReader(file);
-		// fileReader.read(array);
-		// fileReader.close();
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
 
 		BufferedReader br = null;
 
@@ -151,10 +87,11 @@ public class Delete {
 		for (int i = 0; i < items.length; i++) {
 
 			int decimal = Integer.parseInt(items[i].substring(3), 16);
-//			 convert the decimal to character
 			StringBuilder sb = new StringBuilder();
+			// konwersja dziesietnej na znak
 			sb.append((char) decimal);
 			tablica[i] = sb.toString();
+			array[i] = tablica[i].charAt(0);
 		}
 
 		BSONDocument bsonDocument = new BSONDocument();
--- RSO_projekt/src/CRUD/DeleteTest.java
+++ RSO_projekt/src/CRUD/DeleteTest.java
@@ -1,11 +1,5 @@
 package CRUD;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-
 import messages.DeleteMessage;
 
 import org.junit.Before;
@@ -14,15 +8,9 @@ import org.junit.Test;
 import bson.BSON;
 import bson.BSONDocument;
 
-import com.google.gson.Gson;
-import com.mongodb.DBObject;
-import com.mongodb.util.JSON;
-
 public class DeleteTest {
-	Collection<DBObject> collection = new ArrayList<DBObject>();
-	Gson gson = new Gson();
 	DeleteMessage deleteMessage = new DeleteMessage();
-	String directory = "C:/Users/Tomek/Desktop/test/";
+	String directory = "testoweBsony/";
 
 	@Before
 	public void setUp() {
@@ -40,30 +28,11 @@ public class DeleteTest {
 		
 		deleteMessage.fullCollectionName = directory;
 		deleteMessage.selector = doc; 
-
-		// DataObject dataObject1 = new DataObject(1, "pierwszy", 10.89);
-		// String json1 = gson.toJson(dataObject1);
-		//
-		// DataObject dataObject2 = new DataObject(2, "drugi", 18.467);
-		// String json2 = gson.toJson(dataObject2);
-		//
-		// DBObject dbObject1 = (DBObject) JSON.parse(json1);
-		// DBObject dbObject2 = (DBObject) JSON.parse(json2);
-		//
-		// collection.add(dbObject1);
-		// collection.add(dbObject2);
-		// System.out.println(collection);
 	}
 
 	@Test
 	public void test() {
 		Delete deleteObj = new Delete();
-		
 		deleteObj.deteleEntry(deleteMessage);
-
-		// String json =
-		// "{ \"id\" : 5 , \"name\" : \"pi¹ty\" , \"value\" : 8.78}";
-		// deleteObj.deleteObject(collection, 2);
-		// System.out.println(collection);
 	}
 }
