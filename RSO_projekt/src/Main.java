f3197713dbf07f3f15f870d863e1526e0dd95ac9
Piotr Cebulski <pcebulsk@mion.elka.pw.edu.pl>
2013-06-05 02:51:52 +0200

    rmi

--- RSO_projekt/src/Main.java
+++ RSO_projekt/src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 
 import configserver.ConfigServer;
+import configserver.RemClient;
 
 import listener.Listener;
 
@@ -16,32 +17,28 @@ public class Main {
 	public static void main(String[] args) throws IOException
 	{
 		if (args.length == 3) {
+			//mozemy utworzyc serwer konfiguracyjny
 		if (args[0].equals("-start_conf"))
 		{
 			int port = 0;
 			if (args[1].equals("-port"))
 				port = Integer.parseInt(args[2]);
-			System.out.println(args[2]);
 			new ConfigServer(port);
 		}
 		}
 		if (args.length == 4) {
+			//lub podlaczyc sie do istniejacego serwera
 		if (args[0].equals("-conf"))
 		{
 			InetAddress configServer = InetAddress.getByName(args[1]);
 			int port = 0;
 			if (args[2].equals("-port"))
-				port = Integer.getInteger(args[3]);
-			registerToConfigServer(configServer, port);
+				port = Integer.parseInt(args[3]);
+			new RemClient(configServer);
 		}
 		}
-		new Listener(27017);
+//		new Listener(27017);
 		
 	}
 
-	private static void registerToConfigServer(InetAddress configServer,
-			Integer port) {
-		System.out.println("Connecting to config server...");
-		
-	}
 }
